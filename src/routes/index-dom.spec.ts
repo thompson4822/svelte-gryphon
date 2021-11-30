40f2dcea4352da793df08191ba421df8b12084a8
Steve Thompson <thompson4822@protonmail.com>
2021-11-30 14:14:03 -0700

    Extra files

---
+++ src/routes/index-dom.spec.ts
@@ -0,0 +1,36 @@
+import { render, RenderResult } from '@testing-library/svelte';
+import Index from './index.svelte';
+
+/**
+ * @jest-environment jsdom
+ */
+
+/**
+ * An example test suite outlining the usage of
+ * `describe()`, `beforeEach()`, `test()` and `expect()`
+ *
+ * @see https://jestjs.io/docs/getting-started
+ * @see https://github.com/testing-library/jest-dom
+ */
+
+describe('Index', () => {
+
+  let renderedComponent: RenderResult;
+
+  beforeEach(() => {
+    renderedComponent = render(Index);
+  });
+
+  describe('once the component has been rendered', () => {
+
+    test('should show title for the People component', () => {
+      expect(renderedComponent.getByText('Customers')).toBeInTheDocument();
+    });
+
+    test('should show some number of People cards', () => {
+      expect(renderedComponent.getByText('.person')).toBeInTheDocument();
+    });
+
+  });
+
+});
